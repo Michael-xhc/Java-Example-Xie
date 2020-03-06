e30b9f5d5fb59841fd595d1e8893016ab184bdd7
xiehanchun <1243636085@qq.com>
2020-03-06 12:58:51 +0800

    搭建springboot

---
+++ xie-admin/src/main/java/cn/com/sinosafe/xieadmin/HaxbOutsourceXieAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.com.sinosafe.xieadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaxbOutsourceXieAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaxbOutsourceXieAdminApplication.class, args);
+    }
+
+}
---
+++ xie-admin/src/test/java/cn/com/sinosafe/xieadmin/HaxbOutsourceXieAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.sinosafe.xieadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaxbOutsourceXieAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
