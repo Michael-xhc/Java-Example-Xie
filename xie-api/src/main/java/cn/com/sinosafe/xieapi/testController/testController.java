fd20cb14f421c2baab889abf9f29c614659807a9
xiehanchun <1243636085@qq.com>
2020-03-16 18:05:28 +0800

    冒泡排序

--- xie-api/src/main/java/cn/com/sinosafe/xieapi/testController/testController.java
+++ xie-api/src/main/java/cn/com/sinosafe/xieapi/testController/testController.java
@@ -7,7 +7,7 @@
  */
 package cn.com.sinosafe.xieapi.testController;
 
-import java.util.Scanner;
+import javax.sound.midi.Soundbank;
 
 /**
  * @description: 测试
@@ -18,18 +18,97 @@ import java.util.Scanner;
  * @version: v1.0
  **/
 public class testController {
-    public static void main(String[] args) {
-        int[] nums = {2,2,3,4,2,6,2,2,9};
-        System.out.println("数组长度----》"+nums.length/2);
+    /*public static void main(String[] args) {
+        int[] nums = {3,4,7,1,9,6,2,5,11};
+        //3 4 1 7 6 2 5 9 11
+        //3 1 4 6 2 5 7 9 11
+        //1 3 4 2 5 6 7 9 11
+        //1 3 2 4 5 6 7 9 11
+        //1 2 3 4 5 6 7 9 11
+
+        System.out.println("数组长度----》"+nums.length);
        for(int i = 0; i < nums.length; i++){
            int temp = 0;
-           for(int j = 0; j < nums.length-i-1;i++){
-                if(nums[j] > nums[j+1]){
-                    temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = nums[j];
+           for(int j = 0;j < nums.length-i-1;j++) {
+               if (nums[j] > nums[j + 1]) {
+                   temp = nums[j];
+                   nums[j] = nums[j + 1];
+                   nums[j + 1] = temp;
+               }
+           }
+       }
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }*/
+
+    /*public static void main(String[] args) {
+        int[] nums = {3,4,7,1,9,6,2,5,11};
+        //1 4 7 3 9 6 2 5 11
+        //1 2 7 4 9 6 3 5 11
+
+        System.out.println("数组长度----》"+nums.length);
+        for(int i = 0; i < nums.length; i++){
+            int temp = 0;
+            for (int j = i;j < nums.length - 1 -i; j++){
+                if(nums[i] > nums[j]){
+                    nums[i] = temp;
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
             }
+        }
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }*/
+
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 7, 1, 9, 6, 2, 5, 11};
+        //1 3 4 5 6 7 9 11
+        //1 3 4  7 9
+        //已知循环用for 未知循环次数用while
+       /* int t = 0;
+        int n = 0;
+        for(int i = 0;i < nums.length;i++){
+            t = i-1;
+            n = nums[i];
+            // 这里主要关注t在变化
+            while (t >= 0 && nums[t] > n) {
+                nums[t+1] = nums[t];
+                t--;
+            }
+            nums[t+1] = n;
+        }
+
+        for (int num : nums) {
+            System.out.println(num);
+        }*/
+        //3, 4, 7, 1, 9, 6, 2, 5, 11
+       int n = 0;
+       int t = 0;
+        for(int i = 0; i < nums.length; i++){
+           int j = i-1;
+           t =  nums[i];
+           for(; j >= 0; j--) {
+               if(nums[j] > t){
+                   nums[j+1] = nums[j];
+               }
+           }
+           nums[j+1] = t;
+         /* n = i-1;
+          t = nums[i];
+          while(n >= 0 && nums[n] > nums[i]){
+              nums[n+1] = nums[n];
+              n--;
+          }
+          nums[n+1] =nums[i];*/
+
        }
+           for (int num : nums) {
+               System.out.println(num);
+           }
     }
 }
